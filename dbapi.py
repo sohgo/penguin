10c1f9c93308eb8600417b584722c9c562762c03
tanupoo <github-shoichi@tanu.org>
2021-06-09 17:57:16 +0900

    improved to find items. fixed to use JSONResponse, instead of HTMLException. XXX fastapi has something bug ?

--- dbapi.py
+++ dbapi.py
@@ -3,9 +3,10 @@ from fastapi.responses import JSONResponse, Response
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
 from typing import Optional, List, Union
-from modelDB import PenDBStep1Model, PenDBStep2Model
+from modelDB import PenDBStep1Model, PenDBStep2Model, PenDBStep3UpdateModel
 import dateutil.parser
 import motor.motor_asyncio
+from pymongo import ReturnDocument
 
 def api(config):
 
@@ -43,13 +44,16 @@ def api(config):
         # See IMPLEMENTATION.md
         pid = in_data.pid
         xpath = in_data.xpath
-        result = await x_tab.count_documents({"$id":[
+        c3w_words = in_data.c3w_words
+        result = await x_tab.count_documents({"$or":[
                 {"pid":pid},
                 {"xpath":xpath},
+                {"c3w_words":c3w_words},
                 ]})
-        logger.debug(f"count: {result}")
         if result > 0:
-            msg = f"POST STEP1: xpath or pid already exist: xpath={xpath} pid={pid}"
+            logger.debug(f"keys: pid={pid} xpath={xpath} c3w_words={c3w_words}")
+            logger.debug(f"count: {result}")
+            msg = f"POST STEP1: pid, xpath, or c3w_words already exist"
             logger.error(msg)
             raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE,
                                 detail=f"{msg}")
@@ -85,6 +89,23 @@ def api(config):
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                                 detail=f"not found xpath:{xpath}")
 
+    @app.get(
+        "/a/c3ww/{c3w_words}",
+        response_description="Get a single item by c3w_words.",
+        status_code=status.HTTP_200_OK,
+        response_model=PenDBStep2Model
+        )
+    async def get_entry_by_c3ww(xpath: str):
+        logger.debug(f"APP GET c3w_words: {c3w_words}")
+        item = await x_tab.find_one({"c3w_words": c3w_words})
+        logger.debug(f"RET: {item}")
+        if item:
+            item.pop("_id")
+            return item
+        else:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail=f"not found c3w_words: {c3w_words}")
+
     @app.post(
         "/2",
         response_description="update a SPTE1 item for STEP2.",
@@ -94,21 +115,53 @@ def api(config):
     async def post_step2(in_data: PenDBStep2Model = Body(...)):
         logger.debug(f"APP POST STEP2:")
         pid = in_data.pid
-        xpath = in_data.xpath
         # post the info.
         in_json = jsonable_encoder(in_data)
         logger.debug(f"find_one_and_replace: trying: {in_json}")
-        result = await x_tab.find_one_and_replace(
-                {"pid":pid, "xpath":xpath},
+        #result = await x_tab.find_one_and_replace(
+        result = await x_tab.replace_one(
+                {"pid":pid},
                 in_json,
-                upsert=True)
+                upsert=False)
+        if result.modified_count == 0:
+            msg = f"POST STEP2: pid doesn't exist: {pid}"
+            logger.error(msg)
+            return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,
+                                content={"detail": msg})
+        elif result.modified_count > 1:
+            msg = f"POST STEP2: pid matched more than 1 items: {pid}"
+            logger.error(msg)
+            return JSONResponse(
+                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    content={"detail": msg})
         # copy in_data again to avoid leaking "_id".
-        out_json = jsonable_encoder(in_data)
-        if result is not None:
-            return out_json
-        else:
-            return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                                detail=out_json)
+        return jsonable_encoder(in_data)
+
+    @app.post(
+        "/3",
+        response_description="Update tsStep3 for STEP3.",
+        status_code=status.HTTP_200_OK,
+        response_model=PenDBStep2Model,
+        )
+    async def post_step3(in_data: PenDBStep3UpdateModel = Body(...)):
+        logger.debug(f"APP POST STEP3:")
+        pid = in_data.pid
+        ts_step3 = in_data.tsStep3
+        # update the item.
+        logger.debug(f"trying find_one_and_update: {pid}")
+        result = await x_tab.find_one_and_update(
+                {"pid": pid},
+                {"$set": {"tsStep3": ts_step3}},
+                upsert=False,
+                return_document=ReturnDocument.AFTER)
+        if result is None:
+            msg = f"POST STEP3: pid doesn't exist: {pid}"
+            logger.error(msg)
+            return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,
+                                content={"detail": msg})
+        # copy in_data again to avoid leaking "_id".
+        result.pop("_id")
+        return result
 
     @app.get(
         "/1/list",
@@ -119,7 +172,12 @@ def api(config):
     async def list_step1():
         logger.debug(f"GET STEP1 LIST")
         items = await x_tab.find(
-                {"tsStep2":{"$exists":False}}
+                { "$and" : [
+                        { "tsStep1": { "$exists": True } },
+                        { "$or": [
+                                { "tsStep2": { "$exists":False } },
+                                { "tsStep2": None },
+                                ] } ] }
                 ).sort([("tsStep1",-1)]).limit(100).to_list(100)
         logger.debug(f"RET: count={len(items)}")
         if items:
@@ -139,7 +197,12 @@ def api(config):
     async def list_entry():
         logger.debug(f"GET STEP2 LIST")
         items = await x_tab.find(
-                {"tsStep2":{"$ne":None}}
+                { "$and" : [
+                        { "tsStep2": { "$exists": True } },
+                        { "$or": [
+                                { "tsStep3": { "$exists":False } },
+                                { "tsStep3": None },
+                                ] } ] }
                 ).sort([("tsStep2",-1)]).limit(100).to_list(100)
         logger.debug(f"RET: count={len(items)}")
         if items:
@@ -159,7 +222,12 @@ def api(config):
     async def list_entry():
         logger.debug(f"GET STEP3 LIST")
         items = await x_tab.find(
-                {"tsStep3":{"$ne":None}}
+                { "$and" : [
+                        { "tsStep3": { "$exists": True } },
+                        { "$or": [
+                                { "tsStep4": { "$exists":False } },
+                                { "tsStep4": None },
+                                ] } ] }
                 ).sort([("tsStep3",-1)]).limit(100).to_list(100)
         logger.debug(f"RET: count={len(items)}")
         if items:
@@ -186,5 +254,6 @@ def api(config):
         else:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                                 detail=f"not found xpath:{xpath}")
+
     #
     return app
