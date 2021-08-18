a3acc36661a273b013de6dda571820a63d3f66cb
tanupoo <github-shoichi@tanu.org>
2021-08-18 23:48:09 +0900

    added comment about data model and revised to create in/out data.

--- ui/step2/src/Step2Input3.vue
+++ ui/step2/src/Step2Input3.vue
@@ -49,24 +49,24 @@
 
                     <v-container
                         class="ma-0 pa-1"
-                        v-for="(v, i) in input_presence"
+                        v-for="(p, i) in locationList"
                         :key="i"
                     >
                         <v-row>
                             <v-checkbox
-                                :label="v.label"
-                                v-model="v.checked"
+                                :label="p.label"
+                                v-model="p.checked"
                                 dense
                             >
                             </v-checkbox>
                         </v-row>
-                        <template v-if="v.checked">
+                        <template v-if="p.checked">
                             <p>いつ訪れましたか？</p>
                             <v-checkbox
                                 class="ma-0 pa-0"
-                                v-for="(d, i) in v.dateList"
+                                v-for="(d, i) in p.dateList"
                                 :key="i"
-                                :label="d.label"
+                                :label="d.date"
                                 v-model="d.checked"
                                 dense
                                 >
@@ -96,34 +96,53 @@
 <script>
 import utils from '@/common/utils.js'
 
+const focusedLocations = [
+    // 外部から入力
+    // 名称,エリア,クラスター認定日,住所
+    // XXX 外部から読み込めるようにする。
+    { label: '北翔第九病院', area: '石狩', date: '2021-08-04', address: '北海道〇△市〇△□〇△□〇△□〇△□〇△□〇△□' },
+    { label: '養護老人ホームはるか豊潤', area: '胆振', date: '2021-08-09', address: '北海道〇△市〇△□〇△□〇△□〇△□〇△□〇△□' },
+    { label: 'しつげんタクシー', area: '釧路', date: '2021-08-14', address: '北海道〇△市〇△□〇△□〇△□〇△□〇△□〇△□' },
+    { label: '栗田病院', area: '胆振', date: '2021-08-21', address: '北海道〇△市〇△□〇△□〇△□〇△□〇△□〇△□' },
+    { label: '後志総合協会病院', area: '後志', date: '2021-08-24', address: '北海道〇△市〇△□〇△□〇△□〇△□〇△□〇△□' },
+    { label: '緑川新高校', area: '石狩', date: '2021-08-29', address: '北海道〇△市〇△□〇△□〇△□〇△□〇△□〇△□' },
+    { label: '手稲総合病院', area: '石狩', date: '2021-09-03', address: '北海道〇△市〇△□〇△□〇△□〇△□〇△□〇△□' },
+]
+
 export default {
     data() {
         return {
             valid: false,
             formData: {},
-            input_presence: [
-/*
-入力している日付が、クラスター認定日(YYYY-MM-DD)よりも後60日(要確認)ならば表示する。とかする？
-formData = {
-    onsetDate: <YYYY-MM-DD>,
-    presenceData: {
-        '<label>': [ false, true, ..., false ] // 14日分, 0, -1, -2, ..., -13
-        ,...
-    }
-}
+                /*
+                ## presenceRecord
+                - locationsは、クラスター発生箇所のリスト。
+                    + 行っていなければ<LABEL>は存在しない。
+                    + 発症日から遡って14日分の<BOO>値をリストで持つ。
+                        * そこに存在していれば Trueにする。
+                    + 遡る基準日は、input1のonseDate(必須)を使用する。
+                    + XXX 要検討: 発症日が、クラスター認定日(YYYY-MM-DD)
+                    よりも十分後ならならば表示しないとか？
 
-dateList = [ { label: <LABEL>, checked: <BOOL> }, ... ]
-*/
-// 名称,エリア,クラスター認定日,住所
-// XXX 外部から読み込めるようにする。
-{ label: '北翔第九病院', area: '石狩', date: '2021-08-04', address: '北海道〇△市〇△□〇△□〇△□〇△□〇△□〇△□', checked:null, dateList:null },
-{ label: '養護老人ホームはるか豊潤', area: '胆振', date: '2021-08-09', address: '北海道〇△市〇△□〇△□〇△□〇△□〇△□〇△□', checked:null, dateList:null },
-{ label: 'しつげんタクシー', area: '釧路', date: '2021-08-14', address: '北海道〇△市〇△□〇△□〇△□〇△□〇△□〇△□', checked:null, dateList:null },
-{ label: '栗田病院', area: '胆振', date: '2021-08-21', address: '北海道〇△市〇△□〇△□〇△□〇△□〇△□〇△□', checked:null, dateList:null },
-{ label: '後志総合協会病院', area: '後志', date: '2021-08-24', address: '北海道〇△市〇△□〇△□〇△□〇△□〇△□〇△□', checked:null, dateList:null },
-{ label: '緑川新高校', area: '石狩', date: '2021-08-29', address: '北海道〇△市〇△□〇△□〇△□〇△□〇△□〇△□', checked:null, dateList:null },
-{ label: '手稲総合病院', area: '石狩', date: '2021-09-03', address: '北海道〇△市〇△□〇△□〇△□〇△□〇△□〇△□', checked:null, dateList:null },
-                ],
+                formData.presenceRecord = {
+                    locations: {
+                        // 14日分, 0, -1, -2, ..., -13
+                        '<LABEL>': [ <BOOL>, <BOOL>, ..., <BOOL> ]
+                        ,...
+                    }
+                }
+
+                */
+            locationList: undefined, // work place holder for presenceRecoed
+                /*
+                locationList: [
+                    {
+                        label: <LABEL>,
+                        checked: <BOOL>,
+                        dateList: [ { date: <DATE>, checked: <BOOL> }, ... ]
+                    }, ...
+                ]
+                */
         }
     },
     methods: {
@@ -141,13 +160,13 @@ dateList = [ { label: <LABEL>, checked: <BOOL> }, ... ]
         },
         updateFormData: function() {
             if (this.$refs.baseform.validate()) {
-                // copy input_presence back into formData
-                for (let i = 0; i < this.input_presence.length; i++) {
-                    let node = this.input_presence[i]
-                    if (node.checked === true) {
-                        this.formData.presenceData[node.label] = node.dateList.map((x) => x.checked)
+                // copy locationList back into formData
+                for (let i = 0; i < this.locationList.length; i++) {
+                    let place = this.locationList[i]
+                    if (place.checked === true) {
+                        this.formData.locations[place.label] = place.dateList.map((x) => x.checked)
                     } else {
-                        delete(this.formData.presenceData[this.input_presence[i].label])
+                        delete(this.formData.locations[place.label])
                     }
                 }
                 // update formData
@@ -174,17 +193,25 @@ dateList = [ { label: <LABEL>, checked: <BOOL> }, ... ]
     },
     mounted: function() {
         this.formData = this.$store.state.formData
-        /* initialize presenceData and input_presence */
-        if (this.formData.presenceData === undefined) {
-            this.formData.presenceData = {}
+        // initialize and locationList
+        if (this.formData.locations === undefined) {
+            this.formData.locations = {}
         }
-        for (let i = 0; i < this.input_presence.length; i++) {
-            let placename = this.input_presence[i].label
-            this.input_presence[i].checked = this.formData.presenceData[placename] === undefined ? false : true
-            this.input_presence[i].dateList = utils.generatePastDateList(this.formData.onsetDate).map((v,j) => ({
-                label: `${v.local} ${v.annotate}`,
-                checked: this.formData.presenceData[placename] === undefined ? false : this.formData.presenceData[placename][j]
-            }))
+        // create locationList
+        if (this.locaionList === undefined) {
+            this.locationList = []
+            for (let i = 0; i < focusedLocations.length; i++) {
+                let place = focusedLocations[i]
+                this.locationList.push({
+                    label: place.label,
+                    checked: this.formData.locations[place.label] === undefined ? false : true,
+                    dateList: utils.generatePastDateList(this.formData.onsetDate).map((v,j) => (
+                        {
+                            label: `${v.local} ${v.annotate}`,
+                            checked: this.formData.locations[place.label] === undefined ? false : this.formData.locations[place.label][j]
+                        }))
+                })
+            }
         }
     },
 }
